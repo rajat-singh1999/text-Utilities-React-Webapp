2c61a7b53dae767352bc1301105355d6705500d6
Rajat Singh <rsingh1734@gmail.com>
2021-08-21 11:28:48 +0530

    Commit number 6. Commit of update from 20 august.

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -3,13 +3,12 @@ import React from 'react'
 export default function TextForm() {
     return (
         <>
-        <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-            </div>
+        <div className="container my-3">
+            <h1>Enter the text below to analyze!</h1>
             <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea className="form-control" id="my-box" rows="10"></textarea>
+            </div>
+            <button className="btn btn-primary">Convert to Uppercase</button>
         </div>
         </>
     )
