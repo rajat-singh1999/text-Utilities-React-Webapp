ec3fdef561a59c72a9894d4061aa3694e03a0fa1
Rajat Singh <rsingh1734@gmail.com>
2021-08-26 10:19:51 +0530

    Commit 9.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import {useState} from 'react';
 // import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
-function App() {
+function App() { 
+  const [darkMode, setDarkMode] = useState("dark");
   return (
     <>
-      <Navbar title="Text Utils"/>
+      <Navbar title="Text Utils" mode={darkMode}/>
       <div className="container my-3">
       <TextForm/>
       {/* <About/> */}
